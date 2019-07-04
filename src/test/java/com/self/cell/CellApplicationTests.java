03043f51f784d8a3d9b9a78219b9845e7decf926
H.J.Yang <1501587673@qq.com>
2019-07-04 23:20:20 +0800

    add mapper file

--- src/test/java/com/self/cell/CellApplicationTests.java
+++ src/test/java/com/self/cell/CellApplicationTests.java
@@ -1,18 +1,35 @@
 package com.self.cell;
 
+import com.self.cell.product.dao.ProductCategoryMapper;
+import com.self.cell.product.entity.ProductCategory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.annotation.Resource;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class CellApplicationTests {
 
-
+    @Resource
+    private ProductCategoryMapper productCategoryMapper;
 
     @Test
     public void contextLoads() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("unit test");
+
+        Random random = new Random();
+
+        productCategory.setCategoryId(1);
+        productCategory.setCategoryType(random.nextInt());
+        productCategoryMapper.updateByPrimaryKey(productCategory);
     }
 
 }
--- src/test/java/com/self/cell/order/OrderTest.java
+++ src/test/java/com/self/cell/order/OrderTest.java
@@ -1,4 +1,27 @@
 package com.self.cell.order;
 
+import com.self.cell.config.MybatisConfig;
+import com.self.cell.order.entity.OrderDetail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import({MybatisConfig.class})
 public class OrderTest {
+
+    @Resource
+    private OrderDetail orderDetail;
+
+    @Test
+    public void insert() {
+
+
+    }
+
 }
