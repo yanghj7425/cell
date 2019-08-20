737f052151cc82ed68c20a67e38f5377de964698
yanghj7425 <1501587673@qq.com>
2019-08-20 10:47:50 +0800

    add multi db support

---
+++ src/main/java/com/self/sell/modules/user/dao/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.self.sell.modules.user.dao;
+
+import com.self.sell.modules.user.entity.SysUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SysUserMapper extends Mapper<SysUser> {
+}
